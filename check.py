310a743d3ae416116d1a86327e4447a70cd1669f
Marius <mariussangwa@gmail.com>
2026-01-12 18:00:43 +0200

    updating chech.py to allow user input

--- check.py
+++ check.py
@@ -1,6 +1,7 @@
 
-#dat =input("Ener the marks of the students separated by commas:" )
-data = [33,77,99]
+dat =input("Ener the marks of the students separated by spaces:" )
+data = list(map( int,dat.split() ))
+
 for i in range (0,len(data)):
 	if (data[i]>=80):
 		print(f"{data[i]}: Grade A")
