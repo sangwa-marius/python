ebd3426bbce27248f47c65960449c1e6770f76f5
Marius <mariussangwa@gmail.com>
2026-01-12 18:21:32 +0200

    changing the markers of the graph in graph.py

--- graph.py
+++ graph.py
@@ -3,7 +3,7 @@ x = input('Enter the x-values separated by spaces: ');
 y = input('Enter the y-values separated also by spaces: ')
 x_values = list(map(int,x.split() ) )
 y_values = list(map(int,y.split() ) )
-plt.plot (x_values,y_values,color ="green", marker = 'x')
+plt.plot (x_values,y_values,color ="green", marker = 'o')
 plt.title("This is a simple graph")
 plt.xlabel("x values")
 plt.ylabel("Y values")
