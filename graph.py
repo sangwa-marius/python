3d1d8ee9b3880e2ef94e94b452deaf025e8801f5
Marius <mariussangwa@gmail.com>
2026-02-16 22:55:16 +0200

    modified this file

--- graph.py
+++ graph.py
@@ -1,11 +1,40 @@
 import matplotlib.pyplot as plt
-x = input('Enter the x-values separated by spaces: ');
-y = input('Enter the y-values separated also by spaces: ')
-x_values = list(map(int,x.split() ) )
-y_values = list(map(int,y.split() ) )
-plt.plot (x_values,y_values,color ="green", marker = 'o')
-plt.title("This is a simple graph")
-plt.xlabel("x values")
+
+def get_values(prompt):
+    while True:
+        try:
+            values = input(prompt)
+            values_list = list(map(float, values.split()))
+            if len(values_list) == 0:
+                raise ValueError("You must enter at least one value.")
+            return values_list
+        except ValueError:
+            print("Invalid input! Please enter numbers separated by spaces.")
+
+
+x_values = get_values("Enter the x-values separated by spaces: ")
+y_values = get_values("Enter the y-values separated by spaces: ")
+
+
+if len(x_values) != len(y_values):
+    print("Error: The number of x-values and y-values must be the same.")
+    exit()
+
+
+plt.plot(x_values, y_values, color="green", marker='o', linestyle='-', label="Line Plot")
+
+
+plt.scatter(x_values, y_values, label="Data Points")
+
+
+plt.title("This is a Simple Graph")
+plt.xlabel("X values")
 plt.ylabel("Y values")
+plt.grid(True)          
+plt.legend()          
+plt.tight_layout()      
+
+
 plt.show()
+
 
