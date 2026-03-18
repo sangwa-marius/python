ffdc0c798d018add90420186996a69f34b899b68
Marius <mariussangwa@gmail.com>
2026-03-18 20:26:58 +0200

    added product, difference and quoteint functionality to calculator.py

--- calculator.py
+++ calculator.py
@@ -2,5 +2,11 @@ a =int( input("Enter the first number: "))
 b =int(
  input ("Enter the second number: "))
 
-sum = a + b ;
+sum = a + b 
+product = a*b
+difference = a-b
+quotient = a/b
 print (f"The sum of {a} and {b} is {sum}")
+print (f"The product of {a} and {b} is {product}")
+print (f"The difference of {a} and {b} is {difference}")
+print (f"The quotient of {a} and {b} is {quotient}")
