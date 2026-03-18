431f0aa8bd20c925664e91142fe305ded4552acd
Marius <mariussangwa@gmail.com>
2026-03-18 21:08:38 +0200

    added a log system to the write.py file

--- write.py
+++ write.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+with open("log.txt","a")as log:
+	log.write(f"[{datetime.now()}] - User logged in \n")
+
 with open("open.txt","w") as f:
 	f.write("\n New line addedd")
 
