1ffa19fa1716ee1444df549b03042bee4cdd74a1
Marius <mariussangwa@gmail.com>
2026-03-18 20:45:19 +0200

    now lines can be printed and their count

--- file.py
+++ file.py
@@ -6,3 +6,11 @@ with open("calculator.py","r")as f:
 with open("py.py","r") as f:
 	data = f.read()
 	print (data)
+
+
+with open("3a.py","r") as f:
+	data = f.readlines()
+	count = 1
+	for line in data:
+		print(f"{count} {line}")
+		count +=1
